b5fb70cd1f095fa54435ac5426f5bb65fcfffae3
Golo Roden <golo.roden@thenativeweb.io>
2016-12-28 13:27:36 +0100

    Add option to get missing dependencies. (#6)
    * Add option to get missing dependencies.
    
    * Update package.json
    
    * Update and pin dependencies.
    
    * Remove unneeded scripts block.
    
    * Clean up package.json.
    
    * Move the license to package.json.
    
    * Clean up sample code.
    
    * Update documentation.
    
    * Fix gulp version.
    
    * Remove unneeded esnext configuration setting.
    
    * Remove unneeded esnext configuration setting.
    
    * Move module to lib folder.
    
    * Improved .gitignore file.
    
    * Remove JSCS, JSHint, and replace them by ESLint and roboter.

--- README.md
+++ README.md
@@ -1,69 +1,95 @@
 # gulp-depcheck
 
-[depcheck](https://www.npmjs.com/package/depcheck) plugin for gulp.
+gulp-depcheck adds [depcheck](https://www.npmjs.com/package/depcheck) support to Gulp.
 
-[![Dependency Status](https://david-dm.org/mcasimir/gulp-depcheck.svg)](https://david-dm.org/mcasimir/gulp-depcheck)
+## Installation
 
-[![Build Status](https://travis-ci.org/mcasimir/gulp-depcheck.svg?branch=master)](https://travis-ci.org/mcasimir/gulp-depcheck)
-
-## Install
-
-```
-npm i --save-dev gulp-depcheck
+```bash
+$ npm install gulp-depcheck
 ```
 
-## Usage
+## Quick start
 
-``` js
-var depcheck = require('gulp-depcheck');
+First you need to add gulp-depcheck to your project.
 
+```javascript
+const depcheck = require('gulp-depcheck');
+```
+
+Then, you can use the `depcheck` funtion to define Gulp tasks.
+
+```javascript
+gulp.task('depcheck', depcheck());
+```
+
+You may want to exclude some directories from being checked, e.g. the `test` directory. To exclude a directory, provide its name using the `ignoreDirs` property.
+
+```javascript
 gulp.task('depcheck', depcheck({
-  ignoreDirs: [ 'docs', 'build' ],
-  ignoreMatches: ['glob']
+  ignoreDirs: [ 'test' ]
 }));
 ```
 
-### Options
+Additionally, if you want to exclude packages from being checked, provide their names using the `ignoreMatches` property. Please note that you may use glob patterns here, e.g. to exclude all packages whose names start with `eslint-config-`.
 
-All [depcheck](https://www.npmjs.com/package/depcheck) options are supported, see their docs for more.
+```javascript
+gulp.task('depcheck', depcheck({
+  ignoreMatches: [ 'eslint-config-*' ]
+}));
+```
 
-#### Plugin Options
+### Configuring the report
 
-##### depcheck
+By default, gulp-depcheck fails on any issue it finds. From time to time you might want to adjust this, e.g. to allow unused packages in the `devDependencies` section of your `package.json` file, or to only report packages that are missing from your `package.json` file.
 
-Use a different version of depcheck.
+To do so, there are a few options you may hand over to the `depcheck` function to configure things.
 
-Default: `undefined`
+- Set `dependencies` to `false` if you don't want to be notified on unused packages in the `dependencies` section.
+- Set `devDependencies` to `false` if you don't want to be notified on unused packages in the `devDependencies` section.
+- Set `missing` to `false` if you don't want to be notified on used packages that are missing from the `package.json` file.
 
-##### ignoreDirsDefault
+Moreover, gulp-depcheck fails if it has problems to read files or directories. To adjust this, again there are a few options you may hand over to the `depcheck` function.
 
-Prepend some default dirs to ignoreDirs. Set this to `false` or to an empty array to skip.
+- Set `invalidFiles` to `false` if you don't want to be notified on files that can not be accessed or parsed.
+- Set `invalidDirs` to `false` if you don't want to be notiied on directories that can not be accessed.
 
-Default: `['node_modules', 'bower_components']`
+Moreover, gulp-depcheck supports all options of [depcheck](https://www.npmjs.com/package/depcheck). For more details, see its documentation.
 
-##### dependencies
+### Configuring depcheck
 
-Fails and reports on unused dependencies.
+If you want to configure the version of depcheck to use explicitly provide a reference to it using the `depcheck` property.
 
-Default: `true`
+Additionally, you are allowed to set a default value for the directories to ignore. For that use the `ignoreDirsDefault` property and hand over the names of the directories to ignore as an array. The default value is `['node_modules', 'bower_components']`.
 
-##### devDependencies
+## Running the build
 
-Fails and reports on unused devDependencies.
+To build this module use [roboter](https://www.npmjs.com/package/roboter).
 
-NB: setting this to `false` will still perform depcheck against `devDependencies` but
-will ignore result, to completely skip the check use `withoutDev` option from depcheck.
+```bash
+$ bot
+```
 
-Default: `true`
+## License
 
-##### invalidFiles
+The MIT License (MIT)
 
-Fails on file access and parsing errors
+Copyright (c) 2015 Maurizio Casimirri
+Copyright (c) 2016 the native web
 
-Default: `true`
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-##### invalidDirs
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-Fails on directory access errors
-
-Default: `true`
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ test/units/gulpDepcheckTests.js
@@ -0,0 +1,317 @@
+'use strict';
+
+const assert = require('assertthat');
+
+const depcheck = require('../../lib/gulpDepcheck');
+
+suite('gulpDepcheck', () => {
+  test('calls depcheck on the current working directory.', done => {
+    depcheck({
+      depcheck (path) {
+        assert.that(path).is.equalTo(process.cwd());
+        done();
+      }
+    })();
+  });
+
+  test('passes the given options to depcheck.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options).is.equalTo({
+          fakeOpt: 1,
+          ignoreDirs: [ 'node_modules', 'bower_components' ]
+        });
+        done();
+      },
+      fakeOpt: 1
+    })();
+  });
+
+  test('does not pass the depcheck reference to depcheck.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options.depcheck).is.undefined();
+        done();
+      }
+    })();
+  });
+
+  test('does not pass the default ignored directories to depcheck.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options.ignoreDirsDefault).is.undefined();
+        done();
+      }
+    })();
+  });
+
+  test('prefixes the ignored directories with the default ignored directories.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options.ignoreDirs).is.equalTo([ 'node_modules', 'bower_components' ]);
+        done();
+      }
+    })();
+  });
+
+  test('does not concatenate the default ignored directories with the ignored directories if the default directories should be ignored.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options.ignoreDirs).is.equalTo([]);
+        done();
+      },
+      ignoreDirsDefault: false
+    })();
+  });
+
+  test('removes duplicates from the ignored directories.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options.ignoreDirs).is.equalTo([ 'node_modules', 'bower_components' ]);
+        done();
+      },
+      ignoreDirs: [ 'node_modules' ]
+    })();
+  });
+
+  test('concatenates the ignored directories and the default ignored directories.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options.ignoreDirs).is.equalTo([ 'node_modules', 'bower_components', 'a', 'b', 'c' ]);
+        done();
+      },
+      ignoreDirs: [ 'a', 'b', 'c' ]
+    })();
+  });
+
+  test('does not prefix the ignored directories with the ignored directories if the default directories should be ignored.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options.ignoreDirs).is.equalTo([ 'a', 'b', 'c' ]);
+        done();
+      },
+      ignoreDirsDefault: false,
+      ignoreDirs: [ 'a', 'b', 'c' ]
+    })();
+  });
+
+  test('does not pass ignore missing to depcheck.', done => {
+    depcheck({
+      depcheck (path, options) {
+        assert.that(options.ignoreMissing).is.undefined();
+        done();
+      }
+    })();
+  });
+
+  test('fails on unused dependencies.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          dependencies: [ 'a', 'b' ]
+        });
+      }
+    })().
+      then(() => {
+        // Should never happen.
+      }).
+      catch(err => {
+        assert.that(err.message.includes('You have unused dependencies:\n\na,\nb')).is.true();
+        done();
+      });
+  });
+
+  test('fails on unused dev dependencies.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          devDependencies: [ 'a', 'b' ]
+        });
+      }
+    })().
+      then(() => {
+        // Should never happen.
+      }).
+      catch(err => {
+        assert.that(err.message.includes('You have unused dependencies:\n\na,\nb')).is.true();
+        done();
+      });
+  });
+
+  test('fails on missing dependencies.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          missing: { packageA: './a.js', packageB: './b.js' }
+        });
+      }
+    })().
+      then(() => {
+        // Should never happen.
+      }).
+      catch(err => {
+        assert.that(err.message.includes('You have missing dependencies:\n\npackageA,\npackageB')).is.true();
+        done();
+      });
+  });
+
+  test('fails on invalid files.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          invalidFiles: { 'a.js': new Error(), 'b.js': new Error() }
+        });
+      }
+    })().
+      then(() => {
+        // Should never happen.
+      }).
+      catch(err => {
+        assert.that(err.message.includes('Unable to parse some files: a.js, b.js')).is.true();
+        done();
+      });
+  });
+
+  test('fails on invalid directories.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          invalidDirs: { directoryA: new Error(), directoryB: new Error() }
+        });
+      }
+    })().
+      then(() => {
+        // Should never happen.
+      }).
+      catch(err => {
+        assert.that(err.message.includes('Unable to access some dirs: directoryA, directoryB')).is.true();
+        done();
+      });
+  });
+
+  test('does not fail on dependencies if dependencies should not be checked.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          dependencies: [ 'a', 'b' ]
+        });
+      },
+      dependencies: false
+    })().
+      then(() => {
+        done();
+      }).
+      catch(done);
+  });
+
+  test('does not fail on dev dependencies if dev dependencies should not be checked.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          devDependencies: [ 'a', 'b' ]
+        });
+      },
+      devDependencies: false
+    })().
+      then(() => {
+        done();
+      }).
+      catch(done);
+  });
+
+  test('does not fail on missing if missing should not be checked.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          missing: { packageA: './a.js', packageB: './b.js' }
+        });
+      },
+      missing: false
+    })().
+      then(() => {
+        done();
+      }).
+      catch(done);
+  });
+
+  test('does not report dependencies if dependencies should not be checked.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          dependencies: [ 'dep1', 'dep2' ],
+          devDependencies: [ 'devDep1', 'devDep2' ]
+        });
+      },
+      dependencies: false
+    })().
+      then(() => {
+        done();
+      }).
+      catch(err => {
+        assert.that(err.message.includes('dep1')).is.false();
+        assert.that(err.message.includes('dep2')).is.false();
+        assert.that(err.message.includes('devDep1')).is.true();
+        assert.that(err.message.includes('devDep2')).is.true();
+        done();
+      });
+  });
+
+  test('does not report dev dependencies if dev dependencies should not be checked.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          dependencies: [ 'dep1', 'dep2' ],
+          devDependencies: [ 'devDep1', 'devDep2' ]
+        });
+      },
+      devDependencies: false
+    })().
+      then(() => {
+        done();
+      }).
+      catch(err => {
+        assert.that(err.message.includes('dep1')).is.true();
+        assert.that(err.message.includes('dep2')).is.true();
+        assert.that(err.message.includes('devDep1')).is.false();
+        assert.that(err.message.includes('devDep2')).is.false();
+        done();
+      });
+  });
+
+  test('does not fail on invalid files if invalid files should not be checked.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          invalidFiles: { 'a.js': new Error(), 'b.js': new Error() }
+        });
+      },
+      invalidFiles: false
+    })().
+      then(() => {
+        done();
+      }).
+      catch(done);
+  });
+
+  test('does not fail on invalid directories if invalid directories should not be checked.', done => {
+    depcheck({
+      depcheck (path, options, callback) {
+        callback({
+          invalidDirs: { './a': new Error(), './b': new Error() }
+        });
+      },
+      invalidDirs: false
+    })().
+      then(() => {
+        done();
+      }).
+      catch(done);
+  });
+
+  test('does not throw if no options are given.', done => {
+    assert.that(() => {
+      depcheck()();
+    }).is.not.throwing();
+    done();
+  });
+});
